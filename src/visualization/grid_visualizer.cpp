12d3f39669823d1742a71659c1835a0edb6f803d
Venkatraman Narayanan <venkatrn@andrew.cmu.edu>
2016-04-13 21:15:04 -0400

    add the cpp as well

--- src/visualization/grid_visualizer.cpp
+++ src/visualization/grid_visualizer.cpp
@@ -1,6 +1,11 @@
 #include <sbpl_utils/visualization/grid_visualizer.h>
 
-GridVisualizer::GridVisualizer() : height_(0), width_(0) {
+namespace {
+  constexpr int kDisplayImageWidth = 300;
+  constexpr int kDisplayImageHeight = 300;
+}
+
+GridVisualizer::GridVisualizer() : height_(0), width_(0), scaling_factor_x_(1.0), scaling_factor_y_(1.0) {
 
 }
 
@@ -17,13 +22,35 @@ void GridVisualizer::SetGrid(const unsigned char *const *const grid_data, int he
   stateful_grid_ = colored_grid_.clone();
 }
 
+void GridVisualizer::SetGrid(const char *const *const grid_data, int height, int width) {
+  grid_.create(height, width, CV_8UC1);
+	for (int ii = 0; ii < height; ++ii) {
+		for (int jj = 0; jj < width; ++jj) {
+			// SBPL grids are in column-major order.
+			grid_.at<unsigned char>(ii, jj) = static_cast<unsigned char>(grid_data[jj][ii]);
+		}
+	}
+  cv::normalize(grid_, grid_, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  cv::applyColorMap(grid_, colored_grid_, cv::COLORMAP_JET);
+  stateful_grid_ = colored_grid_.clone();
+  if (height < 200 || width < 200) {
+    scaling_factor_x_ = kDisplayImageHeight / height;
+    scaling_factor_y_ = kDisplayImageWidth / width;
+  }
+}
+
 void GridVisualizer::VisualizeState(int x_coord, int y_coord, int red, int green, int blue) {
-  stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[0] = red; 
+  // BGR order
+  stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[0] = blue; 
   stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[1] = green; 
-  stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[2] = blue; 
-  DrawSpecialStates();
-  cv::imshow("expansions", stateful_grid_);
-  cv::waitKey(1);
+  stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[2] = red; 
+}
+
+void GridVisualizer::VisualizeLine(int x1, int y1, int x2, int y2, int red, int green, int blue) {
+  cv::Point p1(x1, y1);
+  cv::Point p2(x2, y2);
+  cv::Scalar color(blue, green, red);
+  cv::line(stateful_grid_, p1, p2, color, 2);
 }
 
 void GridVisualizer::AddSpecialState(int x_coord, int y_coord, int red, int green, int blue) {
@@ -45,6 +72,14 @@ void GridVisualizer::DrawSpecialStates() {
     // stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[1] = green; 
     // stateful_grid_.at<cv::Vec3b>(y_coord, x_coord)[2] = blue; 
   }
+}
+
+void GridVisualizer::Display(int delay) {
+  static cv::Mat scaled_grid;
+  DrawSpecialStates();
+  cv::resize(stateful_grid_, scaled_grid, cv::Size(), scaling_factor_x_, scaling_factor_y_, cv::INTER_LANCZOS4);
+  cv::imshow("expansions", scaled_grid);
+  cv::waitKey(delay);
 }
 
 void GridVisualizer::ClearStates() {
